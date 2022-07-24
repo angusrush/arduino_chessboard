c2c968e24b7301bd070debb8699febc8fc6892b0
Angus Rush <angus.h.rush@hotmail.com>
2022-07-24 22:39:55 +0200

    Add parser, switch to control testing I/O vs real board I/O

--- main.py
+++ main.py
@@ -4,40 +4,61 @@ import serial
 import chess
 import chess.pgn
 import logging
+import argparse
 from movebuilder import *
 
 def main():
-    logging.basicConfig(filename='example.log', level=logging.DEBUG)
+    parser = argparse.ArgumentParser(description='parse arguments' 
+                                     + 'for smart chess board')
+    parser.add_argument('-t', 
+                        '--testing', 
+                        action='store_true', 
+                        help='Testing mode, handles input differently')
+    args = parser.parse_args()
+
+
+    if args.testing:
+        ser = serial.Serial('/dev/pts/2', 9600)
+        board = chess.Board()
+        mb = MoveBuilder(board, ser)
+        print("Board set with following configuration.")
+        print(board)
+    else:
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+        starting_fen = "5rk1/5ppp/8/8/8/8/1q3PPP/Q4RK1 w - - 0 1"
+        board = chess.Board(fen = starting_fen)
+        mb = MoveBuilder(board, ser)
+        print("Set pieces up in the following way, then press B.")
+        print(board)
+        mb.set_up_pieces()
+
+    logging.basicConfig(level=logging.DEBUG, filename='events.log')
     
-    #ser = serial.Serial('/dev/ttyACM0', 9600)
-    ser = serial.Serial('/dev/pts/2', 9600)
-    
-    #starting_fen = "5rk1/5ppp/8/8/8/8/1q3PPP/Q4RK1 w - - 0 1"
-    #board = chess.Board(fen = starting_fen)
-    board = chess.Board()
-    
-    mb = MoveBuilder(board, ser)
-    
+    print("Starting new game.")
     logging.info("Starting new game.")
-    
+
     while True:
         if board.turn:
             to_move = "White"
+            not_to_move = "Black"
         else:
             to_move = "Black"
-    
-        print(to_move + " to move.")
-    
+            not_to_move = "White"
+
+        if board.fullmove_number == 1 and to_move == "White":
+            print(f"{to_move} to move.")
+        else:
+            print(f"{not_to_move}'s move complete. {to_move} to move.")
+            
         move = mb.listen_for_move()
+
         if move == chess.Move.null():
             print("The move was illegal! Please place the board in the position")
             print(board)
             input("then press any key:")
         else:
             board.push(move)
-            print(f"{to_move}'s move complete.")
-            #print(board)
-    
+
         if board.is_checkmate() == True:
             print(f"Checkmate! {to_move} wins!")
             print_game = input("Press y to print the game, anything else to exit")
@@ -45,6 +66,6 @@ def main():
                 game = chess.pgn.Game.from_board(board)
                 print(game)
             break
-    
+
 if __name__ == '__main__':
     main()
