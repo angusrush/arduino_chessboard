c3368dc062edaec23181dbb7eff87708725941aa
Angus Rush <angus.h.rush@hotmail.com>
2022-08-07 05:19:42 +0200

    Updated tests

---
+++ tests/tester.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import serial
+import time 
+import sys
+
+port = '/dev/pts/3'
+
+ser = serial.Serial(port, 9600)
+
+try:
+    file = open(sys.argv[1], "r")
+    events = file.readlines()
+
+    for event in events:
+        ser.write(bytes(event, encoding='utf8'))
+        print(event)
+        time.sleep(0.5)
+except:
+    pass
+
+while True:
+    message = input(port + ": ")
+    ser.write(bytes(message + "\n", encoding='utf8'))
